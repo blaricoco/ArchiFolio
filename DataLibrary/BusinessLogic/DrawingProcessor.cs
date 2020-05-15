6fb41894d5e8d43d805c0e632fdfc34f853dc7e1
blaricoco <blaricov@gmail.com>
2020-05-15 11:42:16 +0100

    Business logic for Profiles, Projects and drawings

---
+++ DataLibrary/BusinessLogic/DrawingProcessor.cs
@@ -0,0 +1,39 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class DrawingProcessor
+    {
+        public static int CreateDrawing(string name, string type, string description, 
+                                        string ImagePath)
+        {
+            DrawingModel data = new DrawingModel
+            {
+                Name = name,
+                Type = type,
+                Description = description,
+                ImagePath = ImagePath
+            };
+
+            string sql = @"insert into dbo.Drawing (Name, Type, Description,
+                                                    ImagePath)
+                            values (@Name, @Type, @Description, @ImagePath);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<DrawingModel> LoadDrawings()
+        {
+            string sql = @"Select Id, Name, Type, Description, 
+                           ImagePath from dbo.Drawing;";
+
+            return SqlDataAccess.LoadData<DrawingModel>(sql);
+        }
+    }
+}
---
+++ DataLibrary/BusinessLogic/ProjectProcessor.cs
@@ -0,0 +1,42 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class ProjectProcessor
+    {
+        public static int CreateProject(string name, string description, string imagePath,
+                                        string location, int budget, string type)
+        {
+            ProjectModel data = new ProjectModel
+            {
+                Name = name,
+                Description = description,
+                ImagePath = imagePath,
+                Location = location,
+                Budget = budget,
+                Type = type
+            };
+
+            string sql = @"insert into dbo.Project (Name, Description, ImagePath,
+                                                    Location, Budget, Type)
+                            values (@Name, @Description, @ImagePath, @Location,
+                                    @Budget, @Type);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<ProjectModel> LoadProjects()
+        {
+            string sql = @"Select Id, Name, Description, ImagePath, 
+                           Location, Budget, Type from dbo.Project;";
+
+            return SqlDataAccess.LoadData<ProjectModel>(sql);
+        }
+    }
+}
